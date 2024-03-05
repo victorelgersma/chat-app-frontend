b35f9444bfeb03c4e1f96b5df6e681962bdae1f5
Victor Elgersma <victor.elgersma41@thehutgroup.com>
2024-03-05 14:08:02 +0000

    styling completed

---
+++ app.js
@@ -0,0 +1,7 @@
+const johnSelectorBtn = document.querySelector("#john-selector");
+const janeSelectorBtn = document.querySelector("#jane-selector");
+
+const chatHeader = document.querySelector(".chat-header");
+const chatMessages = document.querySelector(".chat-messages");
+
+const chatInputForm = document.querySelector(".chat-input");
